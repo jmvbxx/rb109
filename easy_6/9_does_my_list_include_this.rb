bbe8205a5ee2ab1482f249ac26f545348c07f62c
Jason Bornhoft <jmvbxx@gmail.com>
2021-01-31 15:12:35 -0500

    Add easy_6 activities

---
+++ easy_6/9_does_my_list_include_this.rb
@@ -0,0 +1,19 @@
+# Write a method named include? that takes an Array and a search value as arguments.
+# This method should return true if the search value is in the array, false if it
+# is not. You may not use the Array#include? method in your solution.
+
+def include?(array, value)
+  # https://ruby-doc.com/core-2.7.2/Array.html#method-i-find_index
+  !!array.find_index(value)
+end
+
+# def include?(array, value)
+#   array.each { |element| return true if value == element }
+#   false
+# end
+
+p include?([1,2,3,4,5], 3) == true
+p include?([1,2,3,4,5], 6) == false
+p include?([], 3) == false
+p include?([nil], nil) == true
+p include?([], nil) == false
