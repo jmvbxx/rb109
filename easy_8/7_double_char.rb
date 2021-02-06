986c0331900e776c9d4c0f9db5da057bdc205a41
Jason Bornhoft <jmvbxx@gmail.com>
2021-02-06 12:17:15 -0500

    Add easy_8 activities

---
+++ easy_8/7_double_char.rb
@@ -0,0 +1,14 @@
+# Write a method that takes a string, and returns a new string in which every character
+# is doubled.
+
+def repeater(string)
+  result = ''
+  string.each_char do |char|
+    result << char << char
+  end
+  result
+end
+
+p repeater('Hello') == "HHeelllloo"
+p repeater("Good job!") == "GGoooodd  jjoobb!!"
+p repeater('') == ''
